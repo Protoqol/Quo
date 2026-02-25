a76b2e1d8a7d9185c4aacf9f8f12207fbc8593c4
Quinten Schorsij <quinten@protoqol.nl>
2026-02-26 00:50:55 +0100

    (fix) ui for search bar

--- src/components/sidebar.rs
+++ src/components/sidebar.rs
@@ -86,7 +86,7 @@ pub fn SideBar() -> impl IntoView {
                                             type="checkbox"
                                             id="autoGroupToggle"
                                             class="sr-only peer"
-                                            checked
+                                            checked=setting.position
                                         />
                                         <div class="w-9 h-5 bg-slate-700 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-slate-400 after:border-slate-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-indigo-600 peer-checked:after:bg-white"></div>
                                     </div>
