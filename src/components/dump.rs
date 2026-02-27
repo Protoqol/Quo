9057ee4d2617acc0bbde1e8639a674e978a7f6e1
Quinten Schorsij <quinten@protoqol.nl>
2026-02-27 01:51:35 +0100

    (feat) optimisations to datetime formatter, only display date if not today

--- src/components/dump.rs
+++ src/components/dump.rs
@@ -1,7 +1,8 @@
-use chrono::{DateTime, Locale};
-use leptos::prelude::*;
+use chrono::prelude::*;
 use leptos::html;
+use leptos::prelude::*;
 use quo_common::payloads::IncomingQuoPayload;
+use std::string::ToString;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -10,18 +11,40 @@ extern "C" {
     fn highlightElement(el: &leptos::web_sys::Element);
 }
 
+// @TODO configurable formats
+const DATETIME_FORMAT: &str = "%_d %b %H:%M:%S.%3f";
+const TIME_FORMAT: &str = "%H:%M:%S.%3f";
+
+// @TODO configurable locale
+const DEFAULT_LOCALE: Locale = Locale::nl_NL;
+
 #[component]
 pub fn DumpItem(dump: IncomingQuoPayload) -> impl IntoView {
     let code_ref = NodeRef::<html::Code>::new();
 
-    Effect::new(move |_| {
-        match code_ref.get() {
-            Some(el) => highlightElement(&el),
-            None => {}
-        }
-    });
+    fn datetime_format(epoch: i64) -> String {
+        let now: DateTime<Local> = Local::now();
 
-    // @TODO configurable locale
+        // Include date if not today
+        if let Some(chrono_dt) = DateTime::from_timestamp_millis(epoch) {
+            if chrono_dt.date_naive() == now.date_naive() {
+                return chrono_dt
+                    .format_localized(TIME_FORMAT, DEFAULT_LOCALE)
+                    .to_string();
+            }
+
+            return chrono_dt
+                .format_localized(DATETIME_FORMAT, DEFAULT_LOCALE)
+                .to_string();
+        }
+
+        "".to_string()
+    }
+
+    Effect::new(move |_| match code_ref.get() {
+        Some(el) => highlightElement(&el),
+        None => {}
+    });
 
     view! {
         <div class="bg-slate-900 text-white my-4 rounded px-4 py-2">
@@ -30,18 +53,7 @@ pub fn DumpItem(dump: IncomingQuoPayload) -> impl IntoView {
                     <span class="bg-pink-800 rounded text-white px-2 ">
                         {format!(" {}", dump.meta.origin)}
                     </span>
-                    <span>
-                        {format!(
-                            " {}",
-                            match DateTime::from_timestamp_millis(dump.meta.time_epoch_ms) {
-                                Some(time) => {
-                                    time.format_localized("%_d %b %H:%M:%S", Locale::nl_NL)
-                                        .to_string()
-                                }
-                                None => "-".to_string(),
-                            },
-                        )}
-                    </span>
+                    <span>{format!(" {}", datetime_format(dump.meta.time_epoch_ms))}</span>
                     {format!("{}", dump.meta.sender_origin.replace("\\", "/"))}
                 </h2>
             </div>
